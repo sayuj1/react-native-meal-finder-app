dbd7407ed79eccbaa25d315a820f8b0e45dc7883
sayuj sehgal <sayuj.sehgal@gmail.com>
2024-06-22 08:46:53 +0530

    Add Category Screen, Meal Overview Screen and stack navigation

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,31 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = ({ item }) => {
+    const pressHandler = () => {
+      navigation.navigate("MealsOverview", {
+        categoryId: item.id,
+      });
+    };
+    return (
+      <CategoryGridTile
+        title={item.title}
+        color={item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
